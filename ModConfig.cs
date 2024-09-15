cced74484cf536c4fd730db93b36a2288faf1337
rokugin <rokugin@gmail.com>
2024-09-14 23:43:18 -0700

    Gut color override logic as CP handles this in 1.6.9, Remove mod data and content pack loading, Remove unused config settings

--- ModConfig.cs
+++ ModConfig.cs
@@ -1,12 +1,8 @@
 ﻿namespace ColorfulFishPonds {
     internal class ModConfig {
 
-        public bool ModEnabled { get; set; } = true;
-        public bool DyeColors { get; set; } = true;
         public int RequiredPopulation { get; set; } = 2;
-        public bool DisableSingleRecolors { get; set; } = false;
-        public bool Debugging { get; set; } = false;
-        public float PrismaticSpeed { get; set; } = 0.3f;
+        public bool DebugLogging { get; set; } = false;
 
     }
 }
---
+++ Patches/FishPondPatch.cs
@@ -0,0 +1,38 @@
+﻿using StardewModdingAPI;
+using StardewValley.Buildings;
+using StardewValley;
+using StardewValley.GameData.FishPonds;
+
+namespace ColorfulFishPonds.Patches;
+internal class FishPondPatch {
+
+    static IMonitor? Monitor;
+    static ModConfig Config = new();
+
+    public static void Init(IMonitor monitor, ModConfig config) {
+        Monitor = monitor;
+        Config = config;
+    }
+
+    public static void DoFishSpecificWaterColoring_Postfix(FishPond __instance) {
+        try {
+            FishPondData data = __instance.GetFishPondData();
+
+            if (data.WaterColor != null) return;
+
+            if (__instance.currentOccupants.Value >= Config.RequiredPopulation) {
+                var color = ItemContextTagManager.GetColorFromTags(ItemRegistry.Create(ItemRegistry.type_object + __instance.fishType.Value));
+                if (color != null) {
+                    __instance.overrideWaterColor.Value = color.Value;
+                }
+            }
+        }
+        catch (Exception ex) {
+            Monitor!.LogOnce(
+                $"Harmony patch {nameof(DoFishSpecificWaterColoring_Postfix)} encountered an error. Custom fish pond colors might not be applied.\n {ex}",
+                LogLevel.Error);
+            return;
+        }
+    }
+
+}
